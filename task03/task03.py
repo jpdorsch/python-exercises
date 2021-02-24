556e342084d6be224652ba999511f37508e5fb77
Tomas Aliaga <taliaga@cscs.ch>
2021-02-24 14:49:01 +0100

    Remove invalid U+00A0 chars

--- task03/task03.py
+++ task03/task03.py
@@ -5,11 +5,11 @@
 # - Create a `persons2` object from `persons` (i.e. with the same data), but the ages reduced by 10 years each.
 # - Print both person objects.
 
-persons = [
-    {"name":"Roger", "lastName":"Federer", "age": 30},
-    {"name":"Julia", "lastName":"Roberts", "age": 25},
-    {"name":"John", "lastName":"Deacon", "age": 60},
-    {"name":"Tom", "lastName":"Jones", "age": 10} ]
+persons = [
+    {"name":"Roger", "lastName":"Federer", "age": 30},
+    {"name":"Julia", "lastName":"Roberts", "age": 25},
+    {"name":"John", "lastName":"Deacon", "age": 60},
+    {"name":"Tom", "lastName":"Jones", "age": 10} ]
 
 # Add your code here...
 
